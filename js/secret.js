ba6398aa30eadd84e44ba8625e796d0697b4b6ac
grubsaidziggy <156846605+grubsaidziggy@users.noreply.github.com>
2024-02-29 14:27:24 -0700

    Create secret.js

---
+++ js/secret.js
@@ -0,0 +1,9 @@
+var grubber = false;
+while(grubber = false) {}
+
+if(grubber = true){
+  console.log("ACCESS GRANTED");
+  var link = document.createElement("a");
+  link.href = "grubber/";
+  document.body.appendChild(link);
+}
