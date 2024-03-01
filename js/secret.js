c8971861ad8ed6dbc807c30b00ce1f9a184a9f8e
grubsaidziggy <156846605+grubsaidziggy@users.noreply.github.com>
2024-03-01 10:01:47 -0700

    Update secret.js

--- js/secret.js
+++ js/secret.js
@@ -1,16 +1,19 @@
 var check_grub = function(key1,key2){
-  if(key1 == "grub" && key2 == 165) 
-  {
-    console.log("ACCESS GRANTED");
-    var link = document.createElement("a");
-    var linkText = document.createTextNode("SECRET");
-    link.appendChild(linkText);
-    link.title = "SECRET";
-    link.href = "grubber/";
-    document.body.appendChild(link);
-  }
-  else
-  {
-    console.log("ACCESS DENIED");
+  switch(key1,key2){
+    case key1=="grub" && key2==165:
+      console.log("ACCESS GRANTED");
+      var link = document.createElement("a");
+      var linkText = document.createTextNode("SECRET");
+      link.appendChild(linkText);
+      link.title = "SECRET";
+      link.href = "grubber/";
+      document.body.appendChild(link);
+      break;
+    
+
+    //"else" case, dont put any other cases below it
+    case true:
+      console.log("ACCESS DENIED");
+      break;
   }
 };
