bd37a588741eb4d607de690fb5baf61bcd1efba7
grubsaidziggy <156846605+grubsaidziggy@users.noreply.github.com>
2024-03-01 14:30:50 -0700

    Update secret.js

--- js/secret.js
+++ js/secret.js
@@ -1,5 +1,5 @@
 var check_grub = function(key1,key2){
-  console.log("possible work");
+  console.log();
   switch(key1,key2){
     case "grub",165:
       console.log("ACCESS GRANTED");
@@ -17,5 +17,5 @@ var check_grub = function(key1,key2){
       console.log("ACCESS DENIED");
       break;
   }
-  console.log("please work");
+  console.log();
 };
