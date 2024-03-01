ab0f1a5c6aa34795fd0b25a0a4949c35a853d189
grubsaidziggy <156846605+grubsaidziggy@users.noreply.github.com>
2024-03-01 09:16:47 -0700

    Update secret.js

--- js/secret.js
+++ js/secret.js
@@ -1,5 +1,5 @@
 var check_grub = function(key1,key2){
-  if(key1 == "grub") && (key2 == 165){
+  if(key1 == "grub") and (key2 == 165){
     console.log("ACCESS GRANTED");
     var link = document.createElement("a");
     var linkText = document.createTextNode("Grubber");
