7ffb7e05ee8e164a0956cbd49e1dba386b061b9c
grubsaidziggy <156846605+grubsaidziggy@users.noreply.github.com>
2024-03-01 09:30:48 -0700

    Update secret.js

--- js/secret.js
+++ js/secret.js
@@ -1,13 +1,17 @@
 var check_grub = function(key1,key2){
-  if(key1 == "grub") and (key2 == 165){
+  if(key1 == "grub") and (key2 == 165) 
+  {
     console.log("ACCESS GRANTED");
     var link = document.createElement("a");
-    var linkText = document.createTextNode("Grubber");
+    var linkText = document.createTextNode("SECRET");
     link.appendChild(linkText);
-    a.title = "Grubber";
+    link.title = "SECRET";
     link.href = "grubber/";
-    link.value = "SECRET";
     document.body.appendChild(link);
+  }
+  else
+  {
+    console.log("ACCESS DENIED");
   }
 }
  
